4118216bd697746bbeafb421fdc859996de0824a
Samsung_PC <alex.negreba@outlook.com>
2021-04-03 15:28:14 +0700

    Добавьте файлы проекта.

---
+++ Telegram_Bot_ATNG/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace Telegram_Bot_ATNG
+{
+    class Program
+    {
+        private static TelegramBotClient telegram;
+        static void Main(string[] args)
+        {
+            telegram = new TelegramBotClient(Token.token);
+            telegram.StartReceiving();
+            telegram.OnMessage += OnMessageHandler;
+            Console.WriteLine("Started bot");
+            Console.ReadKey();
+            telegram.StopReceiving();
+        }
+
+        private static async void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+            var bot_mes = "Hello";
+            if (message.Text != null)
+            {
+                Console.WriteLine(message.From.Id + message.From.FirstName + message.From.LastName);
+                Console.WriteLine(message.Text);
+                await telegram.SendTextMessageAsync(message.Chat.Id, bot_mes);
+            }
+        }
+    }
+}
---
+++ Telegram_Bot_ATNG/Token.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telegram_Bot_ATNG
+{
+    class Token
+    {
+        public static string token = "1788655151:AAG1UrLAne3pMfAY2CapGog-1vCoiMpvmYQ";
+    }
+}
+
